eb88531d223b18af923f4fd5867dc892cea81b6a
Andy Huang <andyyehoo@gmail.com>
2012-05-29 19:21:26 +0800

    再试验一次

--- hadoop-dragon-examples/src/main/java/org/apache/hadoop/realtime/JobClient.java
+++ hadoop-dragon-examples/src/main/java/org/apache/hadoop/realtime/JobClient.java
@@ -9,6 +9,7 @@ public class JobClient
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
+        System.out.println( "Hello Dragon!");
+        System.out.println( "Hello Dragon!");
     }
 }
